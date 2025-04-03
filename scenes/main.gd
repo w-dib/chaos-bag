4894cf3555bbcdc66f4ac941092198380651a6c4
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2025-04-03 23:57:20 +0400

    Add token levels

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,9 +2,93 @@ extends Node2D
 
 @onready var chaos_bag: Area2D = $ChaosBag
 @onready var timer: Timer = $Timer
+@onready var token: Sprite2D = $Token
+
+var current_difficulty
+
+var easy_tokens: Array[String] = [
+	"res://assets/tokens/token_plus_one.png",
+	"res://assets/tokens/token_plus_one.png",
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/cultist.png",
+	"res://assets/tokens/grave_stone.png",
+	"res://assets/tokens/auto_fail.png",
+	"res://assets/tokens/elder_sign.png",
+]
+
+var standard_tokens: Array[String] = [
+	"res://assets/tokens/token_plus_one.png",
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/token_minus_three.png",
+	"res://assets/tokens/token_minus_four.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/cultist.png",
+	"res://assets/tokens/grave_stone.png",
+	"res://assets/tokens/auto_fail.png",
+	"res://assets/tokens/elder_sign.png",
+]
+
+var hard_tokens: Array[String] = [
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/token_minus_three.png",
+	"res://assets/tokens/token_minus_three.png",
+	"res://assets/tokens/token_minus_four.png",
+	"res://assets/tokens/token_minus_five.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/cultist.png",
+	"res://assets/tokens/grave_stone.png",
+	"res://assets/tokens/auto_fail.png",
+	"res://assets/tokens/elder_sign.png",
+]
+
+var expert_tokens: Array[String] = [
+	"res://assets/tokens/token_zero.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_one.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/token_minus_two.png",
+	"res://assets/tokens/token_minus_three.png",
+	"res://assets/tokens/token_minus_three.png",
+	"res://assets/tokens/token_minus_four.png",
+	"res://assets/tokens/token_minus_four.png",
+	"res://assets/tokens/token_minus_five.png",
+	"res://assets/tokens/token_minus_six.png",
+	"res://assets/tokens/token_minus_eight.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/skull.png",
+	"res://assets/tokens/cultist.png",
+	"res://assets/tokens/grave_stone.png",
+	"res://assets/tokens/auto_fail.png",
+	"res://assets/tokens/elder_sign.png",
+]
 
 func _ready():
 	chaos_bag.connect("clicked", _on_clicked)
+	token.hide()
 
 func _on_clicked():
-	print("Clicked")
+	if not token.visible:
+		token.show()
