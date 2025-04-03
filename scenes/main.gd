76a6a12e507695c51999b8ab5fcabd43af4e865d
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2025-04-04 00:21:35 +0400

    Basic features complete

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var chaos_bag: Area2D = $ChaosBag
 @onready var timer: Timer = $Timer
 @onready var token: Sprite2D = $Token
+@onready var original_scale: Vector2  = token.scale
 
 var current_difficulty: Array[String]
 
@@ -84,15 +85,34 @@ var expert_tokens: Array[String] = [
 	"elder_sign"
 ]
 
+var final_token_key: String
+var cycle_count := 0
+var max_cycles := 12
+
 func _ready():
 	current_difficulty = standard_tokens
 	chaos_bag.connect("clicked", _on_clicked)
+	timer.timeout.connect(_on_timer_timeout)
 	token.hide()
-
+	
 func _on_clicked():
 	current_difficulty.shuffle()
-	var token_key = current_difficulty[0]
-	var texture = token_textures.get(token_key)
-	token.texture = texture
+	final_token_key = current_difficulty[0]
+	cycle_count = 0
+	timer.start()
 	if not token.visible:
 		token.show()
+
+func _on_timer_timeout():
+	if cycle_count < max_cycles:
+		var random_key = current_difficulty[randi() % current_difficulty.size()]
+		token.texture = token_textures.get(random_key)
+		cycle_count += 1
+	else:
+		timer.stop()
+		token.texture = token_textures.get(final_token_key)
+		var tween = get_tree().create_tween()
+		tween.tween_property(token, "scale", Vector2(original_scale * 1.3), 0.1)
+		tween.set_trans(Tween.TRANS_SINE)
+		tween.tween_property(token, "scale", Vector2(original_scale * 0.8), 0.1)
+		tween.tween_property(token, "scale", Vector2(original_scale * 1), 0.1)
